50edab007734b25315f13b14fd4b9f4d7a9ff85d
Gene4kaS <jannasavchenko@gmail.com>
2022-01-01 10:53:08 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import './App.css';
+import { Component } from 'react';
+import PokemonForm from './Components/PokemonForm';
+import { ToastContainer } from 'react-toastify';
+import PokemonInfo from './Components/PokemonInfo';
 
-function App() {
-  return <div className="App">TEST</div>;
+export default class App extends Component {
+  state = {
+    pokemoName: '',
+  };
+  handleFormSubmit = pokemonName => {
+    this.setState({ pokemonName });
+  };
+
+  // componentDidMount() {
+  //   this.setState({ loading: true });
+  //   fetch(' https://pokeapi.co/api/v2/pokemon/ditto')
+  //     .then(res => res.json())
+  //     .then(pokemon => this.setState({ pokemon }))
+  //     .finally(() => this.setState({ loading: false }));
+  // }
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
 }
-
-export default App;
